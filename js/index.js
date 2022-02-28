7426d48fb279a30066e9c5a8f53b5d7ae194c950
1804011 <sharifsharifulislam061@gmail.com>
2022-02-28 22:31:44 +0600

    data fetching & showing error done

--- js/index.js
+++ js/index.js
@@ -7,3 +7,25 @@ const loadData = (url, callBack) => {
 		.then((data) => callBack(data));
 };
 // =============================
+
+$("submit").addEventListener("click", () => {
+	let searchText = $("search").value;
+	searchText = searchText.toLowerCase();
+	$("data").textContent = "";
+	loadData(
+		`https://openapi.programming-hero.com/api/phones?search=${searchText}`,
+		(phones) => {
+			if (phones.status === false) {
+				$("error").style.display = "block";
+				$("search").style.border = "2px solid red";
+			} else {
+				$("error").style.display = "none";
+				$("search").style.border = "2px solid black";
+
+				phones.data.forEach((phone) => {
+					const { brand, image, phone_name, slug } = phone;
+				});
+			}
+		}
+	);
+});
