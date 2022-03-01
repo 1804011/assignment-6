635de72788681e2da455c6736c5864e47f02a389
1804011 <sharifsharifulislam061@gmail.com>
2022-03-01 11:42:23 +0600

    detail info done

--- js/index.js
+++ js/index.js
@@ -26,6 +26,17 @@ const displayPhoneInfo = (phones) => {
 		$("error").style.display = "none";
 		$("search").style.border = "2px solid black";
 		let sz = phones.data.length;
+		const detailDiv = document.createElement("div");
+		detailDiv.id = "detail-info";
+		detailDiv.classList.add(
+			"container",
+			"mx-auto",
+			"mb-5",
+			"mt-3",
+			"d-flex",
+			"justify-content-center"
+		);
+		$("data").appendChild(detailDiv);
 		const div = document.createElement("div");
 		div.id = "phone-info";
 		div.classList.add("container", "border", "rounded-3", "py-5");
@@ -70,8 +81,61 @@ const displayPhoneInfo = (phones) => {
 const displayDetail = (event) => {
 	loadData(
 		`https://openapi.programming-hero.com/api/phone/${event.target.id}`,
-		(data) => {
-			console.log(data);
+		(response) => {
+			console.log(response);
+			const {
+				brand,
+				image,
+				name,
+				mainFeatures = "no-information",
+				others = "no-information",
+				releaseDate = "no release date found",
+			} = response.data;
+			const {
+				chipSet = "no information",
+				displaySize = "no information",
+				memory = "no-information",
+				sensors = "no-informations",
+				storage = "no-informations",
+			} = mainFeatures;
+
+			const card = document.createElement("div");
+			card.classList.add("card", "p-4");
+			card.style = "width: 22rem";
+			$("detail-info").textContent = "";
+			$("detail-info").appendChild(card);
+			card.innerHTML = `<img src="${image}" class="card-img-top" alt="${name}" />
+            <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">
+                   ${brand}
+                </p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">${releaseDate}</li>
+                <li class="list-group-item"><span class="bold"> Chipset:</span>  ${chipSet}</li>
+                <li class="list-group-item"><span class="bold"> Display-size: </span>${displaySize}</li>
+                <li class="list-group-item"><span class="bold"> Memory:</span> ${memory}</li>
+                <li class="list-group-item"><span class="bold"> Sensors:</span> ${sensors.toString()}</li>
+                <li class="list-group-item"><span class="bold"> Storage:</span> ${storage}</li>
+            </ul>`;
+			if (others == "no-information") {
+				const li = document.createElement("li");
+				li.classList.add("list-group-item");
+				li.innerHTML = `<span class="bold"> Others informations:</span> ${others}`;
+				document.getElementsByClassName("list-group")[0].appendChild(li);
+			} else {
+				const li = document.createElement("li");
+				li.classList.add("list-group-item");
+				li.innerHTML = `<span class="bold"> Others informations:</span>`;
+				document.getElementsByClassName("list-group")[0].appendChild(li);
+				for (const prop in others) {
+					const li = document.createElement("li");
+					li.classList.add("list-group-item");
+					li.innerHTML = `<span class="bold"> ${prop}:</span> ${others[prop]}`;
+					document.getElementsByClassName("list-group")[0].appendChild(li);
+				}
+			}
 		}
 	);
 };
